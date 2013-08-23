1045ed4f27ffc45ffddefb2666b1df50e6f64294
daly81 <idaly81@gmail.com>
2013-08-23 10:30:25 -0400

    BankAccounts Ian Daly

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,4 +1,48 @@
 
 public class BankAccount {
+	private int accountNumber; 
+	private String name; 
+	private int accountBalance; 
+	int deduction = (-4); 
+	
+	//Overloaded Constructor 
+	public BankAccount(int number, int balance, String ownerName) { 
+		accountNumber = number;
+		name = ownerName; 
+		accountBalance = balance; 
+	}
+	
+	//Default 
+	public BankAccount() {
+		this(0, 0, "ian");
+		
+	}
+	
 
+	//Accessors
+	public int getNumber() {
+		return accountNumber;
+	}
+	
+	public int getAccountBalance() {
+		return accountBalance + deduction; 
+	}
+	
+	public String getName() {
+		return name; 
+	}
+	
+	//Mutator methods
+	public void setAccountNumber(int newNumber) {
+		accountNumber = newNumber; 
+	}
+	
+	public void setAccountBalance(int newBalance) {
+		accountBalance = newBalance + deduction; 
+	}
+	
+	public void setName(String newName) {
+		name = newName; 
+	}
 }
+	
--- src/TestBankAccount.java
+++ src/TestBankAccount.java
@@ -2,8 +2,24 @@
 public class TestBankAccount {
 
 	public static void main(String[] args) {
+		BankAccount ian = new BankAccount(); 
+		BankAccount john = new BankAccount(); 
+		BankAccount jane = new BankAccount( 78456, 254, "jane");
 		
+		System.out.println(" This is " + ian.getName() + " bank account his account number is " + ian.getNumber() + " and his account balance " + ian.getAccountBalance() + 
+				" with the $4.00 monthly deduction");
 		
+		//Using set methods
+		john.setAccountNumber(89725);
+		john.setAccountBalance(400);
+		BankAccount johnBankAccount = new BankAccount();
+		john.setName("john");
+		
+		System.out.println("This is " + john.getName() + " bank account his account number is " + john.getNumber() + " and his account balance is " + john.getAccountBalance() +
+				" with the $4.00 monthly deduction");
+		
+		System.out.println("This is " + jane.getName() + " bank account his account number is " + jane.getNumber() + " and his account balance is " + jane.getAccountBalance() +
+				" with the $4.00 monthly deduction");
 	}
 
 }
